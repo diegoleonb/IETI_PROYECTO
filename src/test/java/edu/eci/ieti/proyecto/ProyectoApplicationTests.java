8ff8d3cf2d068f45d79d7c1a22788ebb5f4dafc2
Diego Leon <diego.leon-ba@mail.escuelaing.edu.co>
2023-02-23 18:26:15 -0500

    First commit

---
+++ src/test/java/edu/eci/ieti/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
